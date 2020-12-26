aed5955dd89a377119bca0ff047c8fba82587182
Dhirendra Singh <singhdhiru.ds@gmail.com>
2020-12-26 11:51:14 +0530

    first commit

---
+++ lib/Onboarding.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: onboarding(),
+    );
+  }
+}
+
+class onboarding extends StatefulWidget {
+  @override
+  _onboardingState createState() => _onboardingState();
+}
+
+class _onboardingState extends State<onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
